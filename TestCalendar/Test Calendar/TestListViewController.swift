96f941c2be225a29178de45299912faf4fc93d42
tAkuMiNL <takaramono0906@gmail.com>
2017-09-09 06:58:43 +0900

    Cellの中身の途中

--- TestCalendar/Test Calendar/TestListViewController.swift
+++ TestCalendar/Test Calendar/TestListViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class TestListViewController: UIViewController {
+    @IBOutlet weak var testListTitle: UILabel!
+    @IBOutlet weak var testListView: UITableView!
+    @IBOutlet weak var testListCell: UITableViewCell!
+    @IBOutlet weak var ListCellBack: UIView!
+    
+    var names = ["ataso","sazae", "mauso"]
 
+    override func loadView() {
+        super.loadView()
+        self.view.addSubview(statusBar())
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,16 +31,15 @@ class TestListViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension TestListViewController: UITableViewDataSource,UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
     }
-    */
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
 }
