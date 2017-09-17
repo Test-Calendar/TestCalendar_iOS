2c414989db87dde8bead257dea1c6cdc193436b6
KO-YAMAKO <ko.foot1103@gmail.com>
2017-09-17 18:54:27 +0900

    showSubjectの表示ができる

--- TestCalendar/Test Calendar/ShowSubjectViewController.swift
+++ TestCalendar/Test Calendar/ShowSubjectViewController.swift
@@ -23,15 +23,13 @@ class ShowSubjectViewController: UIViewController {
     var event: oneEvent!
     var model = CalendarModel.sharedInstance
     var colors: [UIColor] = [CalendarColor.redColor(), CalendarColor.orangeColor(), CalendarColor.yellowColor(), CalendarColor.darkGreen(), CalendarColor.green(), CalendarColor.lightGreen(),CalendarColor.darkBlue(), CalendarColor.blue(), CalendarColor.lightBlue(), CalendarColor.darkPurple(), CalendarColor.lightPurple()]
-
-//    var color = UIColor()
-//    var data = Study()
-    
     
     @IBOutlet weak var time: UILabel!
+    @IBOutlet weak var subjectLabel: UILabel!
     @IBOutlet weak var watch: WatchView!
     @IBOutlet weak var amButton: WatchButton!
     @IBOutlet weak var pmButton: WatchButton!
+    @IBOutlet weak var collection: UICollectionView!
     
     
     @IBAction func changeToAM(_ sender: Any) {
@@ -48,12 +46,8 @@ class ShowSubjectViewController: UIViewController {
     override func loadView() {
         super.loadView()
         self.view.addSubview(statusBar())
-        getData()
-        setData()
-        
-        time.text = showTime(start: data.startTime, end: data.endTime)
-        
-        watch.addSchedule(events: WatchEvent(color: event.color, start: event.start, end: event.end))
+        getData() //足りないデータの取得
+        setData() //データを使って表示を更新
     }
     
     override func viewDidLoad() {
@@ -71,20 +65,24 @@ extension ShowSubjectViewController{
     
     func getData(){
         //データを検索して探す
-        let predicate = NSPredicate(format: "startTime == %@", "\(event.start as Date)")
-        
+        let predicate = NSPredicate(format: "name == %@ AND startTime == %@", "\(event.name)","\(event.start as Date)")
         model.tasks = model.searchTask(predicate: predicate)
         model.studies = model.searchStudy(predicate: predicate)
         model.tests = model.searchTest(predicate: predicate)
+    
         //データをeventに入れる
-        
         event.notification = model.tasks[0].notification
         event.notification = model.studies[0].notification
         event.notification = model.tests[0].notification
     }
     
     func setData(){
-            }
+        subjectLabel.text = event.name
+        time.text = showTime(start: event.start, end: event.end)
+        //通知の更新
+        collection.reloadData()
+        watch.addSchedule(events: [WatchEvent(color: getColor(color: event.color), start: event.start, end: event.end)])
+    }
 }
 
 
@@ -94,10 +92,14 @@ extension ShowSubjectViewController{
 extension ShowSubjectViewController: UICollectionViewDelegate,UICollectionViewDataSource{
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        //色情報を変える。もし他にあれば切り替えしていいかのダイアログを表示
-        
-        collectionView.reloadData() //色の更新
+        if colors[indexPath.row] != getColor(color: event.color){
+            //色を変えるのかダイアログの表示
+            //色の入れ替えができるか検討
+            //他に使われてたら変更するかダイアログを表示
+        }
+        collection.reloadData() //表示の更新
     }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return colors.count //色の数
     }
@@ -110,10 +112,10 @@ extension ShowSubjectViewController: UICollectionViewDelegate,UICollectionViewDa
         
         view.frame = CGRect(x: 0, y: 0, width: radius * 2, height: radius * 2 )
         view.backgroundColor = colors[indexPath.row]
-//        if color == colors[indexPath.row] {
-//            view.layer.borderColor = CalendarColor.black().cgColor
-//            view.layer.borderWidth = 2.0
-//        }
+        if getColor(color: event.color) == colors[indexPath.row] {
+            view.layer.borderColor = CalendarColor.black().cgColor
+            view.layer.borderWidth = 2.0
+        }
         view.layer.cornerRadius = radius
         view.clipsToBounds = true
         cell.addSubview(view)
