99ac3b1dc5582ff0dc3fc0e666b61a2517ca95f9
KO-YAMAKO <ko.foot1103@gmail.com>
2017-09-23 18:49:16 +0900

    Buttonクラスの整理

--- TestCalendar/Test Calendar/Button.swift
+++ TestCalendar/Test Calendar/Button.swift
@@ -7,3 +7,82 @@
 //
 
 import Foundation
+import MaterialComponents
+import UIKit
+
+
+/*
+ * 「+」ボタンクラス
+ */
+
+protocol AddButtonDelegate {
+    func tapped(type: Bool) //call when button tapped
+}
+
+class AddButton: MDCFloatingButton{
+    
+    var type = true
+    var delegate: AddButtonDelegate?
+    
+    override init(frame: CGRect, shape: MDCFloatingButtonShape) {// コードで呼び出された時
+        super.init(frame: frame, shape: shape)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    internal func setup(){
+        self.setTitle("+", for: .normal)
+        self.sizeToFit()
+        self.addTarget(self, action: #selector(AddButton.tapped(sender:)), for: .touchUpInside)
+        self.titleLabel?.font = UIFont.systemFont(ofSize: CGFloat(20))
+        self.titleLabel?.textColor = .white
+        self.backgroundColor = CalendarColor.black()
+    }
+    
+    func tapped(sender: Any){
+        self.delegate?.tapped(type: type)
+    }
+}
+
+
+
+/*
+ * todo, testのボタンクラス
+ */
+
+protocol AddSmallDelegate {
+    func tapped(tag: Int) //call when button tapped
+}
+
+
+class AddSmallButton: MDCFloatingButton{
+    
+    var buttontag = Int()
+    var delegate: AddSmallDelegate?
+    var name = String()
+    
+    override init(frame: CGRect, shape: MDCFloatingButtonShape) {// コードで呼び出された時
+        super.init(frame: frame, shape: shape)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    internal func setup(){
+        self.setTitle(name, for: .normal)
+        self.sizeToFit()
+        self.addTarget(self, action: #selector(AddSmallButton.smallTapped(sender:)), for: .touchUpInside)
+        self.titleLabel?.font = UIFont.systemFont(ofSize: CGFloat(20))
+        self.titleLabel?.textColor = .white
+        self.backgroundColor = CalendarColor.black()
+    }
+    
+    func smallTapped(sender: Any){
+        self.delegate?.tapped(tag: buttontag)
+    }
+}
--- TestCalendar/Test Calendar/HomeViewController.swift
+++ TestCalendar/Test Calendar/HomeViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 import MaterialComponents
 import Koyomi
 
-class HomeViewController: UIViewController{
 
+
+class HomeViewController: UIViewController{
+    
     @IBOutlet weak var month: UILabel! {
         didSet{
             let currentDateString = calendar.currentDateString(withFormat: "yyyy年MM月")
@@ -29,11 +31,6 @@ class HomeViewController: UIViewController{
             calendar.weeks = ("日", "月", "火", "水", "木", "金", "土")
             calendar.dayPosition = .topLeft
             calendar.calendarDelegate = self
-            let today = Date()
-            var components = DateComponents()
-            components.day = 7
-            let weekLaterDay = Calendar.current.date(byAdding: components, to: today)
-            calendar.select(date: today, to: weekLaterDay)
         }
     }
     
@@ -43,6 +40,7 @@ class HomeViewController: UIViewController{
         let currentDateString = calendar.currentDateString(withFormat: "yyyy年MM月")
         month.text = currentDateString
     }
+
     
     @IBAction func nextMonth(_ sender: Any) {
         calendar.display(in: .next)
@@ -52,19 +50,33 @@ class HomeViewController: UIViewController{
     
     
     fileprivate let invalidPeriodLength = 90
-//    var model = CalendarModel.sharedInstance
+    var model = CalendarModel.sharedInstance
+    var buttonType = true //+ボタンの状態
+    var add = AddButton()
+    var todo = AddSmallButton()
+    var test = AddSmallButton()
+    
     
     override func loadView() {
         super.loadView()
         self.view.addSubview(statusBar())
+        add.frame = CGRect(x: self.view.frame.width * 0.79, y:self.view.frame.height * 0.87 , width: 56, height: 56)
+        todo.frame = CGRect(x: self.view.frame.width * 0.79 + 4, y:self.view.frame.height * 0.87 - 135 , width: 48, height: 48)
+        test.frame = CGRect(x: self.view.frame.width * 0.79 + 4, y:self.view.frame.height * 0.87 - 70, width: 48, height: 48)
+        todo.buttontag = 1
+        test.buttontag = 2
+        self.view.addSubview(add)
     }
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        showTasks()
+        showTests()
+        showStudies()
+        add.delegate = self
+        todo.delegate = self
+        test.delegate = self
     }
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -76,22 +88,17 @@ class HomeViewController: UIViewController{
 
 extension HomeViewController: KoyomiDelegate{
     
-    func setData(){
-        
-    }
-    
     func koyomi(_ koyomi: Koyomi, didSelect date: Date?, forItemAt indexPath: IndexPath) {
         print("このセルが選択されました")
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy/MM/dd"
         print(formatter.string(from: date!))
+        //画面を遷移
     }
-    
     
     func koyomi(_ koyomi: Koyomi, currentDateString dateString: String) {
-        print("tttttttt")
+        print(dateString)
     }
-    
     
     @objc(koyomi:shouldSelectDates:to:withPeriodLength:)
     func koyomi(_ koyomi: Koyomi, shouldSelectDates date: Date?, to toDate: Date?, withPeriodLength length: Int) -> Bool {
@@ -102,3 +109,70 @@ extension HomeViewController: KoyomiDelegate{
         return true
     }
 }
+
+
+extension HomeViewController:AddButtonDelegate, AddSmallDelegate{
+    
+    //+
+    func tapped(type: Bool) {
+        if type == true{
+            
+        }else {
+            
+        }
+    }
+    
+    //small
+    func tapped(tag: Int) {
+        if tag == 1{ //todo
+            //todo 画面遷移
+        }else{ //test
+            //画面遷移
+        }
+    }
+}
+
+
+extension HomeViewController{
+    
+    func showTasks(){
+        var days = [Date]()
+        for task in model.tasks{
+            days.append(task.startTime as Date)
+        }
+        //draw black circle
+        calendar.selectedStyleColor = .red
+        calendar.selectionMode = .multiple(style: .circle)
+        calendar.lineView.height = 0.5
+        calendar.lineView.position = .bottom
+        calendar.select(dates: days)
+        print("show tasks")
+    }
+    
+    
+    func showTests(){
+        for test in model.tests{
+            calendar.selectedStyleColor = getColor(color: test.color)
+            calendar.selectionMode = .single(style: .line)
+            calendar.lineView.height = 0.5
+            calendar.lineView.position = .center
+            calendar.select(date: test.startTime as Date)
+        }
+        print("show tests")
+    }
+    
+    
+    func showStudies(){
+        for study in model.studies{
+            calendar.selectedStyleColor = getColor(color: study.color)
+            calendar.selectionMode = .multiple(style: .background)
+            calendar.select(date: study.startTime as Date)
+        }
+        print("show studies")
+    }
+}
+
+
+
+
+
