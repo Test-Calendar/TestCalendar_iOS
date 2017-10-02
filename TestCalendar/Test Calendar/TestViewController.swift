c04f2712cc41048f51bd1db1861bf28b89df4bf8
KO-YAMAKO <ko.foot1103@gmail.com>
2017-10-03 00:57:15 +0900

    addTestするための画面のautolayoutの作成

--- TestCalendar/Test Calendar/TestViewController.swift
+++ TestCalendar/Test Calendar/TestViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class TestViewController: UIViewController {
     
     let button = BottomButton()
-    var tableViewCells = [TitleTableViewCell(), TypeTableViewCell(), ColorTableViewCell(), NoticeTableViewCell(), TimeTableViewCell(), DeadlineTableViewCell()]
     
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet weak var subjectTextField: UITextField!
@@ -19,6 +18,7 @@ class TestViewController: UIViewController {
     @IBOutlet weak var collection: UICollectionView!
     @IBOutlet weak var hourLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var stack: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,7 +36,86 @@ class TestViewController: UIViewController {
 
 // MARK: - BottomButtonDelegate
 extension TestViewController: BottomButtonDelegate{
+    
     func tapped() {
         print("tapped")
     }
 }
+
+
+// MARK: - DatePickerを扱う関数群
+extension TestViewController{
+    
+    /// DatePickerを追加する関数
+    func addDatePicker(){
+        
+        let index = stack.arrangedSubviews.count - 1
+        let addView = stack.arrangedSubviews[index]
+        let scroll = scrollView
+        let offset = CGPoint(x: (scroll?.contentOffset.x)!, y: (scroll?.contentOffset.y)! + addView.frame.height)
+        let newView = UIView() //DatePickerを作る
+        
+        newView.isHidden = true
+        stack.insertArrangedSubview(newView, at: index)
+        UIView.animate(withDuration: 0.25) {
+            newView.isHidden = true
+            scroll?.contentOffset = offset
+        }
+    }
+    
+    /// DatePickerを閉じる関数
+    func deleteDatePicker(){
+        
+    }
+    
+    private func createPicker(type: Int) -> UIStackView{
+        
+        //pickerを作成
+        let picker = UIDatePicker()
+        let width = stack.frame.width
+        let height = stack.frame.height
+        let positionX = stack.frame.origin.x
+        let positionY = stack.frame.origin.y - height
+        
+        picker.backgroundColor = .white
+        picker.frame = CGRect(x: positionX, y: positionY, width: width, height: height)
+        
+        if type == 0 {
+            picker.addTarget(self, action: #selector(TestViewController.changeDate(sender:)), for: .valueChanged)
+        }else {
+            picker.addTarget(self, action: #selector(TestViewController.changeDate), for: .valueChanged)
+        }
+        
+        //stackViewを作成してpickerを追加
+        let newStack = UIStackView()
+        newStack.axis = .horizontal
+        newStack.alignment = .firstBaseline
+        newStack.distribution = .fill
+        newStack.spacing = 8
+        newStack.addArrangedSubview(picker)
+        return newStack
+    }
+    
+    /// DatePickerで時刻を変更した時にlabelの変更をする関数
+    ///
+    /// - Parameter sender: DatePicker
+    func changeDate(sender: UIDatePicker){
+        let formatter = DateFormatter()
+        formatter.setTemplate(.full)
+        dateLabel.text = formatter.string(from: sender.date)
+        print("changedate")
+    }
+    
+    /// DatePickerで何時間勉強するのかを変えた時にLabelを変更する関数
+    ///
+    /// - Parameter sender: DatePicker
+    func changeTime(sender: UIDatePicker){
+        let formatter = DateFormatter()
+        formatter.setTemplate(.onlyHour)
+        hourLabel.text = formatter.string(from: sender.date)
+        print("changeTime")
+        
+    }
+}
+
+
