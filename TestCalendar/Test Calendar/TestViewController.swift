9d07f2db5c7f9df7918c563ac79d21786843d220
KO-YAMAKO <ko.foot1103@gmail.com>
2017-10-03 20:53:54 +0900

    微調整

--- TestCalendar/Test Calendar/TestViewController.swift
+++ TestCalendar/Test Calendar/TestViewController.swift
@@ -12,17 +12,15 @@ class TestViewController: UIViewController {
     
     let button = BottomButton()
     
-    @IBOutlet var scrollView: UIScrollView!
     @IBOutlet weak var subjectTextField: UITextField!
     @IBOutlet weak var typeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var collection: UICollectionView!
     @IBOutlet weak var hourLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
-    @IBOutlet weak var stack: UIStackView!
     
     override func loadView() {
         super.loadView()
-        self.scrollView.addSubview(statusBar())
+        self.view.addSubview(statusBar())
     }
     
     override func viewDidLayoutSubviews() {
@@ -57,18 +55,18 @@ extension TestViewController{
     /// DatePickerを追加する関数
     func addDatePicker(){
         
-        let index = stack.arrangedSubviews.count - 1
-        let addView = stack.arrangedSubviews[index]
-        let scroll = scrollView
-        let offset = CGPoint(x: (scroll?.contentOffset.x)!, y: (scroll?.contentOffset.y)! + addView.frame.height)
-        let newView = UIView() //DatePickerを作る
-        
-        newView.isHidden = true
-        stack.insertArrangedSubview(newView, at: index)
-        UIView.animate(withDuration: 0.25) {
-            newView.isHidden = true
-            scroll?.contentOffset = offset
-        }
+//        let index = stack.arrangedSubviews.count - 1
+//        let addView = stack.arrangedSubviews[index]
+//        let scroll = scrollView
+//        let offset = CGPoint(x: (scroll?.contentOffset.x)!, y: (scroll?.contentOffset.y)! + addView.frame.height)
+//        let newView = UIView() //DatePickerを作る
+//        
+//        newView.isHidden = true
+//        stack.insertArrangedSubview(newView, at: index)
+//        UIView.animate(withDuration: 0.25) {
+//            newView.isHidden = true
+//            scroll?.contentOffset = offset
+//        }
     }
     
     /// DatePickerを閉じる関数
@@ -76,33 +74,32 @@ extension TestViewController{
         
     }
     
-    private func createPicker(type: Int) -> UIStackView{
+//    private func createPicker(type: Int) -> UIStackView{
         
         //pickerを作成
-        let picker = UIDatePicker()
-        let width = stack.frame.width
-        let height = stack.frame.height
-        let positionX = stack.frame.origin.x
-        let positionY = stack.frame.origin.y - height
-        
-        picker.backgroundColor = .white
-        picker.frame = CGRect(x: positionX, y: positionY, width: width, height: height)
-        
-        if type == 0 {
-            picker.addTarget(self, action: #selector(TestViewController.changeDate(sender:)), for: .valueChanged)
-        }else {
-            picker.addTarget(self, action: #selector(TestViewController.changeDate), for: .valueChanged)
-        }
-        
-        //stackViewを作成してpickerを追加
-        let newStack = UIStackView()
-        newStack.axis = .horizontal
-        newStack.alignment = .firstBaseline
-        newStack.distribution = .fill
-        newStack.spacing = 8
-        newStack.addArrangedSubview(picker)
-        return newStack
-    }
+//        let picker = UIDatePicker()
+//        let width = stack.frame.width
+//        let height = stack.frame.height
+//        let positionX = stack.frame.origin.x
+//        let positionY = stack.frame.origin.y - height
+//        
+//        picker.backgroundColor = .white
+//        picker.frame = CGRect(x: positionX, y: positionY, width: width, height: height)
+//        
+//        if type == 0 {
+//            picker.addTarget(self, action: #selector(TestViewController.changeDate(sender:)), for: .valueChanged)
+//        }else {
+//            picker.addTarget(self, action: #selector(TestViewController.changeDate), for: .valueChanged)
+//        }
+//        
+//        //stackViewを作成してpickerを追加
+//        let newStack = UIStackView()
+//        newStack.axis = .horizontal
+//        newStack.alignment = .firstBaseline
+//        newStack.distribution = .fill
+//        newStack.spacing = 8
+//        newStack.addArrangedSubview(picker)
+//        return newStack
     
     /// DatePickerで時刻を変更した時にlabelの変更をする関数
     ///
