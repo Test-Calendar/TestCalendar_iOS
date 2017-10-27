6f6c85a837f38515ec1d2ce38ce7b70496cba759
KO-YAMAKO <ko.foot1103@gmail.com>
2017-10-27 19:46:09 +0900

    #18 cellのautolayoutの設定

--- TestCalendar/Test Calendar/CalendarCell.swift
+++ TestCalendar/Test Calendar/CalendarCell.swift
@@ -1,14 +1,22 @@
-////
-////  CalendarCollectionViewCell.swift
-////  Test Calendar
-////
-////  Created by 山浦功 on 2017/09/01.
-////  Copyright © 2017年 山浦功. All rights reserved.
-////
+//
+//  CalendarCollectionViewCell.swift
+//  Test Calendar
+//
+//  Created by 山浦功 on 2017/09/01.
+//  Copyright © 2017年 山浦功. All rights reserved.
+//
 //
 import UIKit
 
-class CalendarCell: UICollectionViewCell {
+
+struct CalendarViewModel {
+    var day: String
+    var test: UIColor
+    var colors: [UIColor]
+}
+
+
+class CalendarCell: UICollectionViewCell, UICollectionViewDelegate, UICollectionViewDataSource{
     
     var dayLabel : UILabel!
     var events: UICollectionView!
@@ -25,37 +33,18 @@ class CalendarCell: UICollectionViewCell {
     }
     
     private func commonInit(){
-        events.delegate = self as? UICollectionViewDelegate
-        events.dataSource = self as? UICollectionViewDataSource
-        events.reloadData()
-    }
-}
-
-
-// MARK: - Private
-extension CalendarCell{
-    
-    /// CalendarCellに表示する円形のViewを生成する関数
-    ///
-    /// - Parameter color: cellに表示するものの背景の色をUIColorで渡す
-    /// - Returns: 表示するUIViewを返す
-    func setUp(_ color: UIColor) -> UIView{
-        let view = UIView()
-        let radius = (self.backgroundView?.frame.height)! / 2
-        view.layer.masksToBounds = true
-        view.layer.cornerRadius = radius
-        view.backgroundColor = color
-        return view
-    }
-    
-    func updateData(){
+        setUpView()
+        self.addSubview(dayLabel)
+        self.addSubview(events)
+        events.delegate = self
+        events.dataSource = self
         events.reloadData()
     }
 }
 
 
 // MARK: - UICollectionViewDelegate, UICollectionViewDataSource
-extension CalendarCell: UICollectionViewDelegate, UICollectionViewDataSource{
+extension CalendarCell{
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return colors.count
@@ -73,9 +62,53 @@ extension CalendarCell: UICollectionViewDelegate, UICollectionViewDataSource{
 }
 
 
-
-
-
-
-
-
+// MARK: - Private
+extension CalendarCell{
+    
+    /// 日付を表示するcollectionViewのcellにデータを入れる
+    ///
+    /// - Parameter data: 表示したいデータ
+    func updateCell(_ data: CalendarViewModel){
+        colors = data.colors
+        setTest(data.test)
+        events.reloadData()
+    }
+    
+    /// CalendarCellに表示する円形のUIViewを生成する。
+    /// cellの幅の半分の半径を持つ円形のViewが生成される。
+    ///
+    /// - Parameter color: cellに表示する背景色
+    /// - Returns: 表示するUIView
+    func setUp(_ color: UIColor) -> UIView{
+        let view = UIView()
+        let radius = (self.backgroundView?.frame.height)! / 2
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = radius
+        view.backgroundColor = color
+        return view
+    }
+    
+    /// 日付にテストがある場合に日付ラベルの背景に色をつける
+    ///
+    /// - Parameter color: 新しく表示する背景色
+    func setTest(_ color: UIColor){
+        dayLabel.backgroundColor = color
+        dayLabel.textColor = .white
+    }
+    
+    
+    /// 表示する日付ラベルとテスト及び予定を表示する
+    /// collectionViewのAutolayoutを設定する
+    func setUpView(){
+        self.backgroundView?.addConstraints([
+            Constraint(item: dayLabel, .top, to: self, .height, constant: 10),
+            Constraint(item: dayLabel, .left, to: self, .left, constant: 10),
+            Constraint(item: dayLabel, .right, to: self, .left, constant: 10),
+            Constraint(item: dayLabel, .height, to: self, .height, multiplier: 0.3),
+            Constraint(item: events, .height, to: dayLabel, .bottom, constant: 10),
+            Constraint(item: events, .left, to: self, .left, constant: 10),
+            Constraint(item: events, .right, to: self, .right, constant: 10),
+            Constraint(item: events, .bottom, to: self, .bottom, constant: 10)
+        ])
+    }
+}
